53a9ef55f42803f5b6d45b598472043031a628af
Amar P <63669768+amar2468@users.noreply.github.com>
2021-02-20 16:43:27 +0000

    Update SimpleBanking.c
    Note: This project will be drastically improved.
    Added error checking in the program so that, for e.g. , the user cannot withdraw money if the balance is below 0. Also, some parts of the code were changed because they were slightly incorrect and this commit mainly consists of slight improvements and error checking code.

--- Assignments/SimpleBanking.c
+++ Assignments/SimpleBanking.c
@@ -1,6 +1,7 @@
-/* Program that makes a banking system where a user can create an account, check information, withdraw and deposit money and log out when he/she is done
+/*  Program that allows user to create an account, deposit money, withdraw money, display information and log off.
+    This is a simple banking app that a user can use to easily.
     Author: Amar Plakalo
-    Date: 01 December 2019
+    Date - Updated: 20 February 2021
 */
 
 #include <stdio.h>
@@ -9,7 +10,7 @@ int main()
 {
     // Declare variables needed
     
-    int option,age;
+    int option,age,option_1_completed = 0;
     char name,logout;
     float cashdeposit,cashwithdraw,accountbalance,currentbalance;
     
@@ -32,26 +33,55 @@ do
     {
         case 1: // Create an account
         {
-            printf("\nWhat is your name?\n");
-            getchar();
-            scanf("%c", &name);
-            getchar();
-            getchar();
-           
-            printf("\nHow old are you?\n");
-            getchar();
-            scanf("%d", &age);
-            if(age < 18)
+            if (option_1_completed == 0)
             {
-                printf("\nYou are too young to create an account with us. You must be 18 or higher\n");
-                
-            }
-            else
-            {
-                printf("\nCongrats! You have created your first bank account. Use it wisely and earn a lot of money\n");
-            }
+                printf("\nWhat is your name?\n");
+                getchar();
+                scanf("%c", &name);
+                getchar();
+                getchar();
             
-            break;
+                printf("\nHow old are you?\n");
+                getchar();
+                scanf("%d", &age);
+
+                printf("How much money do you wish to put into your bank account? ");
+                getchar();
+                scanf("%f",&accountbalance);
+
+                if(accountbalance < 0)
+                {
+                    printf("You cannot put deposit a minus number into your account. Only positive numbers: ");
+                    accountbalance = 0;
+                    option_1_completed = 0;
+                    break;
+
+                }
+                else if(accountbalance > 0)
+                {
+                    printf("You successfully put %f into your account. Well done!!!",accountbalance);
+                    option_1_completed = 1;
+                    break;
+                }
+                if(age < 18)
+                {
+                    printf("\nYou are too young to create an account with us. You must be 18 or higher\n");
+                    option_1_completed = 0;
+                    
+                }
+                else
+                {
+                    printf("\nCongrats! You have created your first bank account. Use it wisely and earn a lot of money\n");
+                    option_1_completed = 1;
+                }
+            
+                break;
+            }
+            else if(option_1_completed == 1)
+            {
+                printf("You already created an account. You cannot create one again: ");
+                break;
+            }
             
         }
         
@@ -59,25 +89,40 @@ do
         {
             printf("\nHow much cash do you wish to deposit in your newly created bank account? \n");
             scanf("%f", &cashdeposit);
-            printf("\nTo confirm, You have chosen to deposit %f into your bank account\n", cashdeposit);
-            
-            break;
+            if (cashdeposit < 0)
+            {
+                printf("You cannot deposit a negative number: ");
+                break;
+            }
+            else if(cashdeposit > 0)
+            {
+                printf("\nTo confirm, You have chosen to deposit %f into your bank account\n", cashdeposit);
+                accountbalance = accountbalance + cashdeposit;
+                break;
+            }
         }
         
         case 3: // Cash withdrawal
         {
             printf("\nHow much money do you want to withdraw from your account?\n");
             scanf("%f", &cashwithdraw);
-            printf("\nTo confirm, You wish to withdraw %f from your account\n", cashwithdraw);
-            break;
+            if(accountbalance < 0)
+            {
+                printf("You cannot withdraw the money from the account because the balance is less than 0: ");
+                break;
+            }
+            else if(accountbalance > 0)
+            {
+                printf("\nTo confirm, You wish to withdraw %f from your account\n", cashwithdraw);
+                accountbalance = accountbalance - cashwithdraw;
+                break;
+            }
         }
-        
-        accountbalance = currentbalance + (cashdeposit - cashwithdraw);
+
+    
         
         case 4: // Display account information
         {
-            currentbalance = cashdeposit - cashwithdraw;
-            accountbalance = currentbalance;
             printf("\nYour account balance is %f\n", accountbalance);
             printf("\nAmount deposited: %f\n", cashdeposit);
             printf("\nAmount withdrawn: %f\n", cashwithdraw);
@@ -87,9 +132,29 @@ do
         
         case 5: // Logout
         {
-            printf("\nDo you really want to logout? If yes, press y\n");
-            scanf("%c", &logout);
-            break;
+            if(option_1_completed == 1)
+            { 
+                printf("\nDo you really want to logout? If yes, press y\n");
+                getchar();
+                scanf("%c", &logout);
+                if(logout == 'y' || logout == 'Y')
+                {
+                    printf("Thanks for using the app!!! Press any button to exit: ");
+                    break;
+                }
+                else if(logout == 'n' || logout == 'N')
+                {
+                    printf("Ok. Enter any button to return back to main menu: ");
+                    scanf("%d",&option);
+                }
+            }
+            else if(option_1_completed == 0)
+            {
+                printf("You have not created an account - you cannot log out: \n");
+                printf("Press any button to return back to main menu: ");
+                scanf("%d", &option);
+                
+            }
         }
         
         case 6: // Clear output and display available options
